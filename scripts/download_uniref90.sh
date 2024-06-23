da2db7f7b124fa354b5c26fcbf7ae3bbb749e48d
Harsh <harshagrawal.1312@gmail.com>
2024-06-23 18:54:12 +0100

    fix: update uniref90 to uk link

--- scripts/download_uniref90.sh
+++ scripts/download_uniref90.sh
@@ -31,11 +31,9 @@ fi
 
 DOWNLOAD_DIR="$1"
 ROOT_DIR="${DOWNLOAD_DIR}/uniref90"
-SOURCE_URL="ftp://ftp.uniprot.org/pub/databases/uniprot/uniref/uniref90/uniref90.fasta.gz"
+SOURCE_URL="https://ftp.ebi.ac.uk/pub/databases/uniprot/uniref/uniref90/uniref90.fasta.gz"
 BASENAME=$(basename "${SOURCE_URL}")
 
 mkdir --parents "${ROOT_DIR}"
 aria2c "${SOURCE_URL}" --dir="${ROOT_DIR}"
-pushd "${ROOT_DIR}"
 gunzip "${ROOT_DIR}/${BASENAME}"
-popd
